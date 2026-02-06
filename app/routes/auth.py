c7e8a97304c83f57ab79254578c0467487f867f1
Muhammad Ahmad <muhammad.a.ahmed@gmail.com>
2026-02-06 07:19:40 +0500

    Refactor: Direct PDF function migrated to app/services/pdf_gen

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,6 +1,7 @@
 # app/routes/auth.py
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session, g
 from app import limiter
+from app.services.db import DB_ENGINE
 from app.services.auth import verify_user, get_user_profile, create_user
 from app.services.utils import random_success_message
 from app.services.cache import get_user_profile_cached
