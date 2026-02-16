a0dd1dd21ab4c596a2b4b55a8b83f3dc9651d61b
Muhammad Ahmad <muhammad.a.ahmed@gmail.com>
2026-02-16 23:37:23 +0500

    feat: Added AI into report_services

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 # ============================================================================
 # main.py - COMPLETE FIXED VERSION 29--01-2026 06-02-2026 Refactoring done.
 # ============================================================================
-from flask import render_template, session, redirect, url_for, request, flash, jsonify, g, send_file, make_response, current_app
+#from flask import render_template, session, redirect, url_for, request, flash, jsonify, g, send_file, make_response
+
 import os
 from app import create_app
 
 
-#config.py need to transfer it. currently its in app/__init__.py and app/routes.purchases.py
-
-##CURRENCY_SYMBOLS = {'PKR': 'Rs.', 'USD': '$', 'EUR': '€', 'GBP': '£', 'AED': 'د.إ', 'SAR': '﷼'}
-#also limiter need to be transfered to extention.py from app/__init__.py
-
 app = create_app()
-
 
 
 if __name__ == "__main__":
