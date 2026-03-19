7adfc12421457a9d0cb9b030a4e0548cb7935d05
Muhammad Ahmad <muhammad.a.ahmed@gmail.com>
2026-03-20 01:42:53 +0500

    fix:3 update flask-mail smtp

--- app/__init__.py
+++ app/__init__.py
@@ -47,7 +47,7 @@ def create_app():
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = 'groweasycloud@gmail.com'  # replace
-    app.config['MAIL_PASSWORD'] = 'postgresql://postgres:SBfVfXGPTdjEEoUKOSkaeGVdQcPmNfSk@postgres.railway.internal:5432/railway'     # replace
+    app.config['MAIL_PASSWORD'] = 'f0d2a1aef5314ae499694600271843c0'     # replace
     app.config['MAIL_DEFAULT_SENDER'] = 'groweasycloud@gmail.com'
     mail.init_app(app)
 
